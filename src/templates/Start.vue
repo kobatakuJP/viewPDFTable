d7186531e61b047c5e3c7ccc4d1ad7551a6bcf73
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2019-10-25 18:18:32 +0900

    start screen

---
+++ src/templates/Start.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+/** アプリケーションを表示するためのrouter-viewを置くためだけの人生 */
+export default class Start extends Vue {}
+</script>
+
+<style>
+/* 
+  color from :https://colorhunt.co/palette/159679
+  f0134d
+  ff6f5e
+  f5f0e3
+  40bfc1
+*/
+.vpt-theme {
+  background-color: #f5f0e3;
+  color: #f0134d;
+}
+</style>
---
+++ src/templates/Start.vue.d.ts
@@ -0,0 +1,2 @@
+import Vue from "vue";
+export default class Start extends Vue {}
