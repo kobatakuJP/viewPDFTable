3c2ca655e40933b15a0aa363dcb24a0c0868c5e8
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2019-10-25 18:17:56 +0900

    newline

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,11 @@ module.exports = {
   module: {
     rules: [
       {
-          test: /\.css$/,
-          use: ['style-loader', { loader: 'css-loader'/*, options: { url: false }*/ }]
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader' /*, options: { url: false } */ }
+        ]
       },
       {
         test: /\.tsx?$/,
