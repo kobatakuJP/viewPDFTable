c849a932e3d5a4258e148c7dedd6995da6463569
kobataku <tk_musik@yahoo.co.jp>
2019-12-05 11:06:09 +0900

    #5 unselected

---
+++ src/pages/UnselectedPage.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+/** welcome画面。タイトルとかを置く。 */
+export default class UnselectedPage extends Vue {}
+</script>
+
+<style>
+</style>
